8039510321e8ad1a2c401c6ee1b468c45b08d7a0
Petr Kudela <pkudela@searchtechnologies.com>
2018-03-05 10:51:42 +0100

    Flex, table page

--- ng-client/src/app/table/table.component.ts
+++ ng-client/src/app/table/table.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./table.component.scss']
 })
 export class TableComponent implements OnInit {
+  
+  title = 'app';
+  view: boolean;
+  rows = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  text: string = "Vestibulum erat nulla";
 
   constructor() { }
 
   ngOnInit() {
   }
-
+  
+  toggle(){
+    this.view = !this.view;
+    this.text += this.text;
+  }
 }
